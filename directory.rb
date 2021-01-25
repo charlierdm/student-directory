45965f832ed066103e31a0a8cf2997f2650b08ac
charlierdm <charlie.davismiller@gmail.com>
2021-01-25 17:49:52 +0000

    step_8.6_added_center_method

--- directory.rb
+++ directory.rb
@@ -1,6 +1,6 @@
 def input_students
-	puts "Please enter the name of the students"
-	puts "To finish, just hit return twice"
+	puts "Please enter the name of the students".center(100)
+	puts "To finish, just hit return twice".center(100)
 	# create an empty array
 	students = []
 	# get the first name
@@ -8,13 +8,13 @@ def input_students
 	#while the name is not empty, repeat this code
 	while !name.empty? do
 	  # add the student hash to the array
-	  puts "Please enter the student height in cm"
+	  puts "Please enter the student height in cm".center(100)
 	  height = gets.chomp
-	  puts "Please enter place of birth"
+	  puts "Please enter place of birth".center(100)
 	  birthplace = gets.chomp
 		students << {name: name, cohort: :november, height: height,
 								birthplace: birthplace}
-	  puts "Now we have #{students.count} students"
+	  puts "Now we have #{students.count} students".center(100)
 	  # get another name from the user
 	  name = gets.chomp
 	end
@@ -23,15 +23,15 @@ def input_students
 end
 
 def print_header
-	puts "The students of Villains Academy"
-	puts "-------------"
+	puts "The students of Villains Academy".center(100)
+	puts "-------------".center(100)
 end
 
 def print(students)
 	counter = 0 
   while counter < students.length
 	  students.each_with_index do |student, index|
-	  puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort), height :#{student[:height]}cm, born: #{student[:birthplace]} "
+	  puts "#{index + 1}.#{student[:name]} (#{student[:cohort]} cohort), height :#{student[:height]}cm, born: #{student[:birthplace]}".center(100)
 	  counter += 1
 	  end
   end
@@ -39,7 +39,7 @@ def print(students)
 end
 
 def print_footer(names)
-	puts "Overall, we have #{names.count} great students"
+	puts "Overall, we have #{names.count} great students".center(100)
 end
 # nothing happens until we call the methods
 students = input_students
