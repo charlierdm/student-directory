d0dbec9f284e20759e67333803bd083811ef25d0
charlierdm <charlie.davismiller@gmail.com>
2021-01-26 15:04:35 +0000

    step11_saving_the_data_to_a_file

--- directory.rb
+++ directory.rb
@@ -1,9 +1,8 @@
-@students = []
+@students = [] # an array accessible to all methods.
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
   # get the first name
   name = gets.chomp
   # while the name is not empty, repeat this code
@@ -16,6 +15,41 @@ def input_students
   end
   # return the array of students
   @students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+	 puts "1. Input the students"
+   puts "2. Show the students"
+   puts "3. Save the lists to students.csv"
+   puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+    	save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
 end
 
 def print_header
@@ -33,36 +67,16 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-def print_menu
-	 puts "1. Input the students"
-   puts "2. Show the students"
-   puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
-  
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
   end
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you mean, try again"
-  end
+  file.close
 end
 
 #nothing happens until we call the methods
